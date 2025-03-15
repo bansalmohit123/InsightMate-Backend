62025d6837d14afe813c7a1774b5bf4fe1d63bd8
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2025-03-16 01:33:38 +0530

    refactor: remove serverless handler and configuration files, enable local server listening

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.get("/", (req, res, next) => {
   });
   
 const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports.handler = serverless(app);
+
