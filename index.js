63d2e4b898d491e57ccc0cee57b074decb8ac050
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2025-03-17 00:21:14 +0530

    fix: update server listening configuration to bind to all interfaces on port 3000

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.get("/", (req, res, next) => {
   });
   
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
